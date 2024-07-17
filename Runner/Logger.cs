76b89a9c8514f95ba45029f40d3fc18ffd3bbfe3
mvishok <vishokmanikantan@gmail.com>
2024-07-17 11:46:03 +0530

    Updated logger to use ansiconsole

--- Runner/Logger.cs
+++ Runner/Logger.cs
@@ -1,47 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Spectre.Console;
 
 namespace FSDE
 {
     internal class Logger
     {
-        string NL = Environment.NewLine;
-        string NORMAL = Console.IsOutputRedirected ? "" : "\x1b[39m";
-        string RED = Console.IsOutputRedirected ? "" : "\x1b[91m";
-        string GREEN = Console.IsOutputRedirected ? "" : "\x1b[92m";
-        string YELLOW = Console.IsOutputRedirected ? "" : "\x1b[93m";
-        string BLUE = Console.IsOutputRedirected ? "" : "\x1b[94m";
-        string MAGENTA = Console.IsOutputRedirected ? "" : "\x1b[95m";
-        string CYAN = Console.IsOutputRedirected ? "" : "\x1b[96m";
-        string GREY = Console.IsOutputRedirected ? "" : "\x1b[97m";
-        string BOLD = Console.IsOutputRedirected ? "" : "\x1b[1m";
-        string NOBOLD = Console.IsOutputRedirected ? "" : "\x1b[22m";
-        string UNDERLINE = Console.IsOutputRedirected ? "" : "\x1b[4m";
-        string NOUNDERLINE = Console.IsOutputRedirected ? "" : "\x1b[24m";
-        string REVERSE = Console.IsOutputRedirected ? "" : "\x1b[7m";
-        string NOREVERSE = Console.IsOutputRedirected ? "" : "\x1b[27m";
-
         public void Info(string message)
         {
-            Console.WriteLine($"{CYAN}{BOLD}INFO {NOBOLD}{message}{NORMAL}");
+            AnsiConsole.MarkupLine("[cyan bold]INFO[/] {0}", message);
         }
 
         public void Error(string message)
         {
-            Console.WriteLine($"{RED}{BOLD}ERROR {NOBOLD}{message}{NORMAL}");
+            AnsiConsole.MarkupLine("[red bold]ERROR[/] {0}", message);
         }
 
         public void Warning(string message)
         {
-            Console.WriteLine($"{YELLOW}{BOLD}WARNING {NOBOLD}{message}{NORMAL}");
+            AnsiConsole.MarkupLine("[yellow bold]WARNING[/] {0}", message);
         }
 
         public void Success(string message)
         {
-            Console.WriteLine($"{GREEN}{BOLD}SUCCESS {NOBOLD}{message}{NORMAL}");
+            AnsiConsole.MarkupLine("[green bold]SUCCESS[/] {0}", message);
         }
     }
 }
